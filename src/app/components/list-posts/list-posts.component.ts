0241ca9dde9077b2f4e7c769005a169be7ce8cfb
Manal-Hizoun <hizounmanal15@gmail.com>
2025-01-09 00:04:02 +0100

    modif_list-post.component.html/ts

--- src/app/components/list-posts/list-posts.component.ts
+++ src/app/components/list-posts/list-posts.component.ts
@@ -3,31 +3,38 @@ import { CommonModule } from '@angular/common';
 import { PostsService } from '../../services/posts.service';
 import { Post } from '../../interfaces/post';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-list-posts',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './list-posts.component.html',
-  styleUrl: './list-posts.component.scss'
+  styleUrl: './list-posts.component.scss',
 })
 export class ListPostsComponent {
   postsService = inject(PostsService);
   postList: Post[] = [];
-  constructor(){
+  deletionMessage: string | null = null; 
+
+  constructor() {
     this.postsService.getPosts().then((posts: Post[]) => {
       this.postList = posts;
-    })
-
-    //this.postList = this.postService.getPosts();
+    });
   }
-    // Nouvelle méthode pour supprimer un post
-    async deletePost(postId: number) {
-      try {
-        await this.postsService.deletePost(postId); // Appel au service pour supprimer le post
-        this.postList = this.postList.filter(post => post.id !== postId); // Mise à jour locale
-      } catch (error) {
-        console.error('Error deleting post:', error);
-        alert('Failed to delete the post. Please try again.');
-      }
+
+  async deletePost(postId: number) {
+    try {
+      await this.postsService.deletePost(postId);
+      this.postList = this.postList.filter(post => post.id !== postId);
+      const now = new Date();
+      this.deletionMessage = `Post avec ID ${postId} supprimé à ${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
+
+      setTimeout(() => {
+        this.deletionMessage = null;
+      }, 5000);
+    } catch (error) {
+      console.error('Error deleting post:', error);
+      alert('Failed to delete the post. Please try again.');
     }
+  }
 }
